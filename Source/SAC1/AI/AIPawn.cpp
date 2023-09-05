b8d627bab5189be793a93da5553fc8e33a41ff90
WI-Jeong <cpdls23@naver.com>
2023-09-05 14:03:24 +0900

    Unreal Engin C++ SpawnPoint

--- Source/SAC1/AI/AIPawn.cpp
+++ Source/SAC1/AI/AIPawn.cpp
@@ -2,6 +2,7 @@
 
 
 #include "AIPawn.h"
+#include "AISpawnPoint.h"
 
 // Sets default values
 AAIPawn::AAIPawn()
@@ -17,14 +18,31 @@ AAIPawn::AAIPawn()
 	mMesh->SetupAttachment(mBody);
 	mMovement->SetUpdatedComponent(mBody);
 
+	mBody->SetCollisionProfileName(TEXT("AI"));
+	
+	mMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
 }
 
+
+void AAIPawn::SetSpawnPoint(AAISpawnPoint* SpawnPoint)
+{
+	mSpawnPoint = SpawnPoint;
+}
 
 // Called when the game starts or when spawned
 void AAIPawn::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void AAIPawn::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	if (IsValid(mSpawnPoint))
+		mSpawnPoint->ClearObject();
 }
 
 // Called every frame
@@ -35,4 +53,12 @@ void AAIPawn::Tick(float DeltaTime)
 	AddMovementInput(GetActorForwardVector());
 
 }
+
+void AAIPawn::SetCollisionProfile(const FName& Name)
+{
+	mBody->SetCollisionProfileName(Name);
+}
+
+
+
 
--- Source/SAC1/AI/AIPawn.h
+++ Source/SAC1/AI/AIPawn.h
@@ -24,15 +24,32 @@ protected:
 
 	UPROPERTY(Category = Component, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<UFloatingPawnMovement> mMovement;
+	
+	//생성된 스폰포인트를 알고 있게 해준다.
+	//스폰포인트를 이용해서 생성한 객체가 아닐경우 None이 들어가있다.
+	TObjectPtr<class AAISpawnPoint> mSpawnPoint;
 
+public:
+	float GetHalfHeight() const
+	{
+		return mBody->GetScaledCapsuleHalfHeight(); 
+	}
+	
+	void SetSpawnPoint(class AAISpawnPoint* SpawnPoint);
 	
 protected:
 	 //Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	//생성한 객체가 제거될 때 호출된다.
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason);
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+public:
+	void SetCollisionProfile(const FName& Name);
 
 };
  
